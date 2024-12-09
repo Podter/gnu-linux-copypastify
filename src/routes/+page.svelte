6de08ba627e3376f26d861fe30678a030f38a4e6
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-12-09 23:23:16 +0700

    basic ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,40 @@
 <script>
 	import { useCompletion } from '@ai-sdk/svelte';
+	import DicesIcon from 'lucide-svelte/icons/dices';
+	import SquarePenIcon from 'lucide-svelte/icons/square-pen';
+
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+	import { Textarea } from '$lib/components/ui/textarea';
+	import { GNU_COPYPASTA } from '$lib/constants';
 
 	const { completion, input, handleSubmit } = useCompletion();
 </script>
 
-<form on:submit={handleSubmit}>
-	<input name="prompt" bind:value={$input} id="input" />
-	<button type="submit">Submit</button>
-</form>
-<textarea value={$completion} readonly></textarea>
+<main
+	class="mx-auto flex min-h-svh w-full max-w-4xl flex-col items-center justify-center space-y-6 p-6"
+>
+	<div class="space-y-3 text-center">
+		<h1 class="text-2xl font-bold sm:text-3xl md:text-4xl">GNU/Linux Copypastify</h1>
+		<p class="text-sm font-medium text-muted-foreground">
+			Convert any text into GNU/Linux copypasta
+		</p>
+	</div>
+	<form class="flex w-full max-w-sm flex-col items-center space-y-3" on:submit={handleSubmit}>
+		<Input bind:value={$input} name="prompt" id="input" placeholder="Enter your text here" />
+		<div class="grid grid-cols-2 gap-2">
+			<Button type="submit">
+				<SquarePenIcon size={16} class="mr-2" /> Generate
+			</Button>
+			<Button type="button" variant="secondary">
+				<DicesIcon size={16} class="mr-2" /> Randomize
+			</Button>
+		</div>
+	</form>
+	<Textarea
+		value={$completion}
+		readonly
+		placeholder={`${GNU_COPYPASTA.slice(0, 199)}...`}
+		class="h-80"
+	></Textarea>
+</main>
